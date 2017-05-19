293e77248c32853fa3cfb4f06f6caac0711e8fe4
ElisDN <mail@elisdn.ru>
2017-05-19 16:11:54 +0300

    Added product values

---
+++ console/migrations/m170519_183925_create_shop_values_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+class m170519_183925_create_shop_values_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('{{%shop_values}}', [
+            'product_id' => $this->integer()->notNull(),
+            'characteristic_id' => $this->integer()->notNull(),
+            'value' => $this->string(),
+        ]);
+
+        $this->addPrimaryKey('{{%pk-shop_values}}', '{{%shop_values}}', ['product_id', 'characteristic_id']);
+
+        $this->createIndex('{{%idx-shop_values-product_id}}', '{{%shop_values}}', 'product_id');
+        $this->createIndex('{{%idx-shop_values-characteristic_id}}', '{{%shop_values}}', 'characteristic_id');
+
+        $this->addForeignKey('{{%fk-shop_values-product_id}}', '{{%shop_values}}', 'product_id', '{{%shop_products}}', 'id', 'CASCADE', 'RESTRICT');
+        $this->addForeignKey('{{%fk-shop_values-characteristic_id}}', '{{%shop_values}}', 'characteristic_id', '{{%shop_characteristics}}', 'id', 'CASCADE', 'RESTRICT');
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%shop_values}}');
+    }
+}
