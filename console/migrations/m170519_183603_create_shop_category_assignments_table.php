0abda17a0aea6c32f4db3269b1c2cefb650fc36b
ElisDN <mail@elisdn.ru>
2017-05-19 15:46:33 +0300

    Added product categories

---
+++ console/migrations/m170519_183603_create_shop_category_assignments_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+class m170519_183603_create_shop_category_assignments_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('{{%shop_category_assignments}}', [
+            'product_id' => $this->integer()->notNull(),
+            'category_id' => $this->integer()->notNull(),
+        ]);
+
+        $this->addPrimaryKey('{{%pk-shop_category_assignments}}', '{{%shop_category_assignments}}', ['product_id', 'category_id']);
+        
+        $this->createIndex('{{%idx-shop_category_assignments-product_id}}', '{{%shop_category_assignments}}', 'product_id');
+        $this->createIndex('{{%idx-shop_category_assignments-category_id}}', '{{%shop_category_assignments}}', 'category_id');
+
+        $this->addForeignKey('{{%fk-shop_category_assignments-product_id}}', '{{%shop_category_assignments}}', 'product_id', '{{%shop_products}}', 'id', 'CASCADE', 'RESTRICT');
+        $this->addForeignKey('{{%fk-shop_category_assignments-category_id}}', '{{%shop_category_assignments}}', 'category_id', '{{%shop_categories}}', 'id', 'CASCADE', 'RESTRICT');
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%shop_category_assignments}}');
+    }
+}
