63b49f351469d4b602853ad65190e65fec11c4f3
ElisDN <mail@elisdn.ru>
2017-05-19 18:40:09 +0300

    Added product modifications
    Added product modifications

---
+++ console/migrations/m170519_212344_create_shop_modifications_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+class m170519_212344_create_shop_modifications_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('{{%shop_modifications}}', [
+            'id' => $this->primaryKey(),
+            'product_id' => $this->integer()->notNull(),
+            'code' => $this->string()->notNull(),
+            'name' => $this->string()->notNull(),
+            'price' => $this->integer(),
+        ]);
+
+        $this->createIndex('{{%idx-shop_modifications-code}}', '{{%shop_modifications}}', 'code');
+        $this->createIndex('{{%idx-shop_modifications-product_id-code}}', '{{%shop_modifications}}', ['product_id', 'code'], true);
+        $this->createIndex('{{%idx-shop_modifications-product_id}}', '{{%shop_modifications}}', 'product_id');
+
+        $this->addForeignKey('{{%fk-shop_modifications-product_id}}', '{{%shop_modifications}}', 'product_id', '{{%shop_products}}', 'id', 'CASCADE', 'RESTRICT');
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%shop_modifications}}');
+    }
+}
