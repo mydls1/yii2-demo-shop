96c8f74625e396cb2a059fe3c09222458898af9a
ElisDN <mail@elisdn.ru>
2017-05-19 16:27:18 +0300

    Added product tags

---
+++ console/migrations/m170519_203816_create_shop_tag_assignments_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+class m170519_203816_create_shop_tag_assignments_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('{{%shop_tag_assignments}}', [
+            'product_id' => $this->integer()->notNull(),
+            'tag_id' => $this->integer()->notNull(),
+        ]);
+
+        $this->addPrimaryKey('{{%pk-shop_tag_assignments}}', '{{%shop_tag_assignments}}', ['product_id', 'tag_id']);
+
+        $this->createIndex('{{%idx-shop_tag_assignments-product_id}}', '{{%shop_tag_assignments}}', 'product_id');
+        $this->createIndex('{{%idx-shop_tag_assignments-tag_id}}', '{{%shop_tag_assignments}}', 'tag_id');
+
+        $this->addForeignKey('{{%fk-shop_tag_assignments-product_id}}', '{{%shop_tag_assignments}}', 'product_id', '{{%shop_products}}', 'id', 'CASCADE', 'RESTRICT');
+        $this->addForeignKey('{{%fk-shop_tag_assignments-tag_id}}', '{{%shop_tag_assignments}}', 'tag_id', '{{%shop_tags}}', 'id', 'CASCADE', 'RESTRICT');
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%shop_tag_assignments}}');
+    }
+}
