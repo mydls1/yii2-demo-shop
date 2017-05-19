8fb1a990ab18e932649ee6baa2e617436cbb559c
ElisDN <mail@elisdn.ru>
2017-05-19 16:31:50 +0300

    Added product relateds
    Added product relateds

---
+++ console/migrations/m170519_203840_create_shop_related_assignments_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+class m170519_203840_create_shop_related_assignments_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('{{%shop_related_assignments}}', [
+            'product_id' => $this->integer()->notNull(),
+            'related_id' => $this->integer()->notNull(),
+        ]);
+
+        $this->addPrimaryKey('{{%pk-shop_related_assignments}}', '{{%shop_related_assignments}}', ['product_id', 'related_id']);
+
+        $this->createIndex('{{%idx-shop_related_assignments-product_id}}', '{{%shop_related_assignments}}', 'product_id');
+        $this->createIndex('{{%idx-shop_related_assignments-related_id}}', '{{%shop_related_assignments}}', 'related_id');
+
+        $this->addForeignKey('{{%fk-shop_related_assignments-product_id}}', '{{%shop_related_assignments}}', 'product_id', '{{%shop_products}}', 'id', 'CASCADE', 'RESTRICT');
+        $this->addForeignKey('{{%fk-shop_related_assignments-related_id}}', '{{%shop_related_assignments}}', 'related_id', '{{%shop_products}}', 'id', 'CASCADE', 'RESTRICT');
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%shop_related_assignments}}');
+    }
+}
